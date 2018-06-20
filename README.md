8ce6cd37270bdc4911a94e6011a35bc25b087aba
Chyroc <chen_yunpeng@foxmail.com>
2018-06-20 20:39:40 +0800

    :star: done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # golang-wasm-example
+
+## build wasm
+```
+GOARCH=wasm GOOS=js /path/go/bin/go build -o example.wasm wasm.go
+```
+
---
+++ wasm.go
@@ -0,0 +1,32 @@
+// +build js,wasm
+
+package main
+
+import (
+	"syscall/js"
+	"strconv"
+)
+
+func main() {
+	var numberDoc = js.Global.Get("document").Call("getElementById", "number")
+	var plus = js.Global.Get("document").Call("getElementById", "plus")
+	var minus = js.Global.Get("document").Call("getElementById", "minus")
+	var number int
+
+	plus.Call("addEventListener", "click", js.NewCallback(func(args []js.Value) {
+		println("press +")
+		number++
+		numberDoc.Set("innerHTML", strconv.Itoa(number))
+	}))
+
+	minus.Call("addEventListener", "click", js.NewCallback(func(args []js.Value) {
+		number--
+		numberDoc.Set("innerHTML", strconv.Itoa(number))
+		println("press -")
+	}))
+
+	plus.Set("disabled", false)
+	minus.Set("disabled", false)
+
+	select {}
+}
